6b077cb6d06adc1802fc21317a4615bc93acfb2c
Hniii98 <752608928@qq.com>
2026-04-16 15:33:19 +0800

    fix: fix errors

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,6 +1,6 @@
-from pretokenization import parallel_pre_tokenization
-from merge import merge_pairs
-from utils import AutoKeyDictWrapper
+from .pretokenization import parallel_pre_tokenization
+from .merge import merge_pairs
+from .utils import AutoKeyDictWrapper
 
 def train_bpe(
 	input_path: str,
@@ -10,15 +10,15 @@ def train_bpe(
 		   	list[tuple[bytes, bytes]]]:
 	
 	freq_map = parallel_pre_tokenization(input_path=input_path, 
-									  	 num_process=4,
+									  	 num_processes=8, # num of processor
 									  	 special_tokens=special_tokens)
 	
 	initial_vocab = AutoKeyDictWrapper()
 	
 	bytes_chars = [bytes([n]) for n in range(256)] 
-	bytes_special_token = [bytes([token]) for token in special_tokens]
+	bytes_special_tokens = [bytes(token.encode("utf-8")) for token in special_tokens]
 
-	initial_vocab.add_list(bytes_special_token + bytes_chars)
+	initial_vocab.add_list(bytes_special_tokens + bytes_chars)
 
 	merges = merge_pairs(freq_map=freq_map, 
 					  	 initial_vocab=initial_vocab.dict, 
