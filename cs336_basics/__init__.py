d7f428416a93d4a44da90c7889ce351f003090cb
Hniii98 <752608928@qq.com>
2026-04-17 14:51:01 +0800

    feat: implement gpt2 stype byte remap fucntion and wrap it

--- cs336_basics/__init__.py
+++ cs336_basics/__init__.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
-from .train_bpe import train_bpe
+from .train_bpe import train_bpe, encode_bytes, bytes_to_unicode
 try:
     __version__ = importlib.metadata.version("cs336_basics")
 except importlib.metadata.PackageNotFoundError:
--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,6 +1,28 @@
 from .pretokenization import parallel_pre_tokenization
 from .merge import merge_pairs
 from .utils import AutoKeyDictWrapper
+import os
+
+
+def bytes_to_unicode() -> dict[int: str]:
+	valid_bytes = list(range(33, 127)) + list(range(161, 173)) + list(range(174, 256))
+	unicode_map = valid_bytes[:]
+
+	n = 0
+	for i in range(256):
+		if i not in valid_bytes:
+			valid_bytes.append(i)
+			unicode_map.append(256 + n)
+			n +=1
+	
+	unicode_map = [chr(c) for c in unicode_map]
+	return dict(zip(valid_bytes, unicode_map))
+
+
+def encode_bytes(token_bytes: bytes,
+				 encoder: dict[int: str]) -> str:
+	return "".join(encoder[b] for b in token_bytes)
+
 
 def train_bpe(
 	input_path: str,
@@ -10,8 +32,10 @@ def train_bpe(
 ) -> tuple[ dict[int, bytes],
 		   	list[tuple[bytes, bytes]]]:
 	
+	num_processor = os.cpu_count() # including logical core
+	
 	freq_map = parallel_pre_tokenization(input_path=input_path, 
-									  	 num_processes=8, # num of processor
+									  	 num_processes=num_processor, 
 									  	 special_tokens=special_tokens)
 	
 	initial_vocab = AutoKeyDictWrapper()
