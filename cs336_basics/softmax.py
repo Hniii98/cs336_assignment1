517de1c84b8718d20fff87974878380b565fbd5f
Hniii98 <752608928@qq.com>
2026-05-13 16:03:27 +0800

    fix: fix typo

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -7,10 +7,10 @@ def softmax(
 	"""
 		softmax(x_i) = exp(x_i) / sum(exp(x_i))
 	"""
-	row_max = torch.max(x, dim=i, keepdim=True).values
+	row_max = torch.max(x, dim=dim, keepdim=True).values
 	x_shifted = x - row_max
 	exp_shifted = torch.exp(x_shifted)
-	row_sum = torch.sum(exp_shifted, dim=i, keepdim=True)
+	row_sum = torch.sum(exp_shifted, dim=dim, keepdim=True)
 
 	normalized_x = exp_shifted / row_sum
 
